583c6f54f949b5f4910acb229c4f0a98940d7811
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-04-13 05:47:28 +0200

    Initial project setup

---
+++ src/main/java/com/operations/ship/ShipOperationsServerApplication.java
@@ -0,0 +1,13 @@
+package com.operations.ship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipOperationsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipOperationsServerApplication.class, args);
+	}
+
+}
